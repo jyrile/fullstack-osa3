819cdf3cf16766290e54f9f5dcc481a00c052366
Jyri Lehtinen <jyri.lehtinen@outlook.com>
2020-05-18 13:39:22 +0300

    osa3 production

---
+++ build/precache-manifest.5851ac24ce6ac580909d292d812d6798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8984525587d8a41df74c974e4f77f8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "678f2b4f9dab0f290e9f",
+    "url": "/static/css/main.130e765e.chunk.css"
+  },
+  {
+    "revision": "96fa2a80d0443e24a6d9",
+    "url": "/static/js/2.eb02a478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb02a478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678f2b4f9dab0f290e9f",
+    "url": "/static/js/main.c507f07c.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
---
+++ build/precache-manifest.c3f01e4bce6b8912ff5b12cbeb6c8fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8056d8c2baf5096eabb47585b4d6717c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "404fd2173ebc9d968cd2",
+    "url": "/static/css/main.130e765e.chunk.css"
+  },
+  {
+    "revision": "96fa2a80d0443e24a6d9",
+    "url": "/static/js/2.eb02a478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb02a478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404fd2173ebc9d968cd2",
+    "url": "/static/js/main.6801c1dd.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
